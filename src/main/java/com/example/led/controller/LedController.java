6284af5f9a4021c931a904f2a47dd168ac883393
Marco Gordillo <marcopgordillo@gmail.com>
2018-06-28 16:27:41 -0500

    "PIN added"

---
+++ src/main/java/com/example/led/controller/LedController.java
@@ -0,0 +1,29 @@
+package com.example.led.controller;
+
+import com.pi4j.io.gpio.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LedController {
+
+    private static GpioPinDigitalOutput pin;
+
+    @RequestMapping("/")
+    public String greeting(){
+        return "Hello World!!!!";
+    }
+
+    @RequestMapping("/light")
+    public String light(){
+
+        if (pin == null) {
+            GpioController gpio = GpioFactory.getInstance();
+            pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_18, "MyLED", PinState.LOW);
+        }
+
+        pin.toggle();
+
+        return "OK";
+    }
+}
