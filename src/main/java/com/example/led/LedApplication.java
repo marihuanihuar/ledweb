565665448904c3281e633af60a7471e70ddcbb8f
Marco Gordillo <marcopgordillo@gmail.com>
2018-06-28 16:07:43 -0500

    Spring created

---
+++ src/main/java/com/example/led/LedApplication.java
@@ -0,0 +1,12 @@
+package com.example.led;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LedApplication.class, args);
+    }
+}
